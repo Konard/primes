5070c8de8bd6b48193ae5e4c10d0fee5736883f1
Konstantin Dyachenko <drakonard@gmail.com>
2024-05-23 23:09:11 +0000

    Experiment 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// EXPERIMENT 1
+
 // use primes::{Sieve, PrimeSet};
 
 // fn main() {
@@ -10,16 +12,41 @@
 //     }
 // }
 
-use primes::{Sieve, PrimeSet};
+// EXPERIMENT 2
+
+// use primes::{Sieve, PrimeSet};
+
+// fn main() {
+//     let limit = 1000; // You can change this limit to any number you want
+//     let mut pset = Sieve::new();
+//     let primes: Vec<u64> = pset.iter().take_while(|&p| p <= limit).collect();
+
+//     for pair in primes.windows(2) {
+//         if let [prev_prime, current_prime] = pair {
+//             println!("p[n] - p[n-1] - 1 for primes {} and {} is {}", current_prime, prev_prime, current_prime - prev_prime - 1);
+//         }
+//     }
+// }
+
+use primes::{Sieve, PrimeSet, is_prime};
 
 fn main() {
-    let limit = 1000000; // You can change this limit to any number you want
+    let limit = 1000; // You can change this limit to any number you want
     let mut pset = Sieve::new();
     let primes: Vec<u64> = pset.iter().take_while(|&p| p <= limit).collect();
 
     for pair in primes.windows(2) {
         if let [prev_prime, current_prime] = pair {
-            println!("p[n] - p[n-1] - 1 for primes {} and {} is {}", current_prime, prev_prime, current_prime - prev_prime - 1);
+            let difference = current_prime - prev_prime - 1;
+            let is_difference_prime = is_prime(difference) || difference == 1;
+
+            println!(
+                "Difference (p[n] - p[n-1] - 1) for primes {} and {} is {}. Is it prime? {}",
+                current_prime,
+                prev_prime,
+                difference,
+                is_difference_prime
+            );
         }
     }
 }
